0bab8d5f1e4f64810ef76ef6f3bd6072644e6483
Hao-Ping Ho <Jas@hao-pings-mbp.wv.cc.cmu.edu>
2014-09-03 17:43:32 -0400

    transactional output and input v1

--- src/TransactionalFileOutputStream.java
+++ src/TransactionalFileOutputStream.java
@@ -1,14 +1,48 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.Serializable;
 
-
+/*
+ *  TODO: Sync between write file and migrate thread.   by haoping
+ */
 public class TransactionalFileOutputStream extends OutputStream implements Serializable{
 
-  @Override
-  public void write(int arg0) throws IOException {
-    // TODO Auto-generated method stub
+	private int offset;
+	private String filename;
+	
+	/* "Cache" the connection in FileInputStream */
+	FileOutputStream mOutput;
+	/* means "safe" when no migration */
+	boolean safe = true;
+	
+	public TransactionalFileOutputStream(String fname) {
+		offset = 0;
+		filename = fname;
+		
+		try {
+			mOutput = new FileOutputStream(filename);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void write(int arg0) throws IOException {
+		if (safe) {
+			mOutput.write(arg0);
+		}
+		else {
+			mOutput = new FileOutputStream(filename);
+			mOutput.write(arg0);
+			
+			safe = true;
+		}
     
-  }
+		offset++;
+	}
 
 }
