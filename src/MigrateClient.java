37cc2f06a158c45260d477638fc473dfb9f69791
Hao-Ping Ho <Jas@hao-pings-mbp.wv.cc.cmu.edu>
2014-09-03 11:48:57 -0400

    start files

---
+++ src/MigrateClient.java
@@ -0,0 +1,34 @@
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+
+public class MigrateClient implements Runnable{
+	private Socket mSocket;
+	
+	public MigrateClient(Socket s) {
+		mSocket = s;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			
+			Scanner in = new Scanner(mSocket.getInputStream());
+			PrintWriter out = new PrintWriter(mSocket.getOutputStream());
+			
+			while(true) {
+				if (in.hasNext()) {
+					
+					String msgIn = in.nextLine();
+					
+					
+				}
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/MigrateMatser.java
@@ -0,0 +1,10 @@
+
+public class MigrateMatser implements Runnable{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
